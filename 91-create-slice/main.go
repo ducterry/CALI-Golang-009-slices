c1e182fcce9f7601ab30a029623a6c64ba10779d
Nguyen Dang Duc <dev.ndangduc.bn@gmail.com>
2021-07-22 10:17:56 +0700

    Add code 91

---
+++ 91-create-slice/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+/*
+	- Ngày: 22.07.2021
+	- By: Nguyễn Đăng Đức
+*/
+func main() {
+
+	// 01. Khai báo 1 Slice
+	var (
+		mySlice = []int{3, 5, 7, 9, 11, 13, 17}
+	)
+
+	// 02. In màn hình
+	fmt.Println("mySlice: ", mySlice)
+}
